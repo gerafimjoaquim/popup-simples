fe51868f318f6bf4bad7bdc9048efe403143ab92
Gerafim Joaquim <oi@gerafim.com>
2023-01-29 14:57:12 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,30 @@
+const btnOpenPopup = query('.btn-open-popup')
+const popup = query('.popup-wrapper')
+
+const openPopup = () => {
+	popup.style.display = 'block'
+}
+
+const closePopup = event => {
+	const clickedElement = event.target
+	const classNamesForClosing = ['popup-wrapper', 'btn-close', 'social-networks']
+	const clickedElementClassname = clickedElement.classList[0]
+	const clickedElementIsASocialNatworkButton = clickedElementClassname === 'social-networks'
+	
+	const classNamesMatch = classNamesForClosing.some(className =>
+		className === clickedElementClassname)
+
+	if (classNamesMatch) {
+		popup.style.display = 'none'
+	}
+
+	if (clickedElementIsASocialNatworkButton) {
+		const socialNetworLink = 'https://www.telegram.com/gerafimjoaquim'
+
+		setAttribute(clickedElement, 'target', '_blank')
+		setAttribute(clickedElement, 'href', socialNetworLink)
+	}
+}
+
+btnOpenPopup.addEventListener('click', openPopup)
+popup.addEventListener('click', closePopup)
